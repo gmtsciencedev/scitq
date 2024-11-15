d448ef4b74a488a6338cf8ff4280070f0259320d
Raynald de Lahondes <raynald@delahondes.com>
2024-11-15 19:41:52 +0100

    add uri_get_samples and guess_library_layout_and_filter functions in scitq.bio.genetics

--- src/scitq/bio/genetics.py
+++ src/scitq/bio/genetics.py
@@ -5,6 +5,9 @@ import subprocess
 import io
 import csv
 import re
+from scitq.fetch import list_content
+import statistics
+import math
 
 DEPTH_REGEXP=re.compile(r"(?P<pair1>[12]x)?(?P<core>\d+[kmg]?)(?P<pair2>x[12])?")
 
@@ -31,7 +34,7 @@ result=read_run&fields=all&format=json&download=true&limit=0"
             samples[run.sample_accession].append(run)
     return samples
 
-def sra_get_samples(bioproject:str, LibraryLayout:Optional[str]=None, LibraryStrategy:Optional[str]=None) -> Dict[str,List[Namespace]]:
+def sra_get_samples(bioproject:str, library_layout:Optional[str]=None, library_strategy:Optional[str]=None) -> Dict[str,List[Namespace]]:
     """Get a list of runs grouped by sample, optionally filtered by library_layout (paired/unpaired for instance) 
     and or by library_strategy (WGS/amplicon for instance)
     The run objects listed have a uri attribute that can conveniently be used as input in scitq tasks"""
@@ -43,22 +46,130 @@ def sra_get_samples(bioproject:str, LibraryLayout:Optional[str]=None, LibraryStr
             raise BioException('docker command was not found and is required for sra_get_samples')
         else:
             raise error
-            
-    output=io.StringIO(p.stdout)
+    
+    if len(p.stdout.split())==0:
+        raise BioException(f'No such bioproject {bioproject}')
+
+    header_line,body = p.stdout.split(maxsplit=1)
+    new_style_headers = []
+
+    # fixing SRA headers to make them more consistent with ENA and python style
+    for header in header_line.split(','):
+        words=[word[:-1].lower() if word.endswith('_') else word.lower() for word in re.findall('[A-Z]?[^A-Z]+|[A-Z]+', header)]
+        new_style_headers.append('_'.join(words) if words else header) 
+               
+    output=io.StringIO(f"{','.join(new_style_headers)}\n{body}")
     samples = {}
     for item in csv.DictReader(output, delimiter=','):
         run=Namespace(**item)
-        if LibraryStrategy and run.LibraryStrategy!=LibraryStrategy:
+        if library_strategy and run.library_strategy!=library_strategy:
             continue
-        if LibraryLayout and run.LibraryLayout!=LibraryLayout:
+        if library_layout and run.library_layout!=library_layout:
             continue
-        run.uri=f'run+fastq@sra://{run.Run}'
-        if run.BioSample not in samples:
-            samples[run.BioSample]=[run]
+        run.uri=f'run+fastq@sra://{run.run}'
+        run.sample_accession = run.bio_sample
+        if run.bio_sample not in samples:
+            samples[run.bio_sample]=[run]
         else:
-            samples[run.BioSample].append(run)
+            samples[run.bio_sample].append(run)
     return samples
 
+def count(items: List[str], ending: str) -> int:
+    """Count how many items end with ending"""
+    return len([item for item in items if item.endswith(ending)])
+
+def uri_get_samples(uri: str, ending: str='.fastq.gz', alternate_endings: List[str]=['.fastq','.fq.gz','.fq']) -> Dict[str,List[Namespace]]:
+    """Get a list of runs grouped by sample organized within the folder uri,
+     this function tries to guess the organization of files, grouped by folder or common prefix, 
+     it also tries to infer some info like library_layout"""
+    samples = {}
+    project_files = [file for file in list_content(uri) if file.rel_name.endswith(ending)]
+    candidate_endings=iter(alternate_endings)
+    try:
+        while len(project_files)==0:
+            ending = next(candidate_endings)
+            project_files = [file for file in list_content(uri) if file.rel_name.endswith(ending)]
+    except StopIteration:
+        raise BioException(f'{uri} does not seems to contains sample files')
+    
+    # try grouped by folder
+    for file in project_files:
+        sample = file.name.split('/')[-2]
+        if sample not in samples:
+            samples[sample]=[]
+        samples[sample].append(file.name)
+    
+    folder_grouped = True
+    if len(project_files)/len(samples) > 3:
+        folder_grouped = False
+    else:
+        file_topology = [len(files) for files in samples.values()]
+        if len(file_topology)>1 and statistics.stdev(file_topology)>statistics.mean(file_topology):
+            folder_grouped = False
+    
+    # try to group by common part 
+    if not folder_grouped:
+        group_by_common_prefix = False
+        common_prefix_len = math.floor(statistics.mean([len(file.rel_name) for file in project_files])) - len(ending)
+        while common_prefix_len>0:
+            candidate_samples = {}
+            for file in project_files:
+                sample = file.rel_name[:common_prefix_len]
+                if sample not in candidate_samples:
+                    candidate_samples[sample]=[]
+                candidate_samples[sample].append(file.name)
+            if len(project_files)/len(candidate_samples)<1.5:
+                common_prefix_len-=1
+                continue
+            file_topology = [len(files) for files in candidate_samples.values()]
+            if len(file_topology)>1 and statistics.stdev(file_topology)>statistics.mean(file_topology):
+                common_prefix_len-=1
+                continue
+            group_by_common_prefix = True
+            break
+        if group_by_common_prefix:
+            print(f'Samples seem to be grouped by common prefix of length {common_prefix_len}')
+            samples = candidate_samples
+        else:
+            samples={}
+            common_prefix_len = math.floor(statistics.mean([len(file.rel_name) for file in project_files])) - len(ending)
+            for file in project_files:
+                sample = file.rel_name[:common_prefix_len]
+                if sample not in samples:
+                    samples[sample]=[]
+                samples[sample].append(file.name)
+            print('Could not find any grouping possibility')
+    else:
+        print('Samples seem to be grouped by folder')
+
+    final_samples = {}
+    for sample, files in samples.items():
+        if len(files)%2==0 and count(files,f'2{ending}')==count(files,f'1{ending}'):
+            library_layout = 'PAIRED'
+        else:
+            library_layout = 'UNPAIRED'
+        final_samples[sample] = [Namespace(sample_accession=sample, uri=file, library_layout=library_layout) for file in files]
+    return final_samples
+
+def guess_library_layout_and_filter(samples: Dict[str,List[Namespace]]) -> Tuple[str,Dict[str,List[Namespace]]]:
+    """Look for the most likely dominant library_layout and return a tuple consisting of 
+     the guessed library_layout and a filtered set of samples with runs filtered by the guessed layout """
+    paired=unpaired=0
+    for sample,runs in samples.items():
+        unpaired_likeliness=0
+        for run in runs:
+            if run.library_layout=='UNPAIRED':
+                unpaired_likeliness+=1
+            elif run.library_layout=='PAIRED':
+                paired+=1
+                break
+        else:
+            if unpaired_likeliness>0:
+                unpaired+=1
+    if unpaired==paired or (unpaired==0 and paired==0):
+        raise BioException(f'Cannot decide: paired vote {paired} vs unpaired vote {unpaired}')
+    library_layout = 'PAIRED' if paired>unpaired else 'UNPAIRED'
+    return library_layout, {sample:[run for run in runs if run.library_layout==library_layout] for sample,runs in samples.items()}
 class Depth:
     """A simple object providing minimal information of the type of sequencing"""
     
